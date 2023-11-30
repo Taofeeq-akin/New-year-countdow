a69f9608998977c08adcb56bedf8f285000ffdf2
Taofeeq akin <taofeeqomoakin18@gmail.com>
2023-11-30 15:03:57 +0100

    Add spinner before countdoen display

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
 const nextYear = document.getElementById("nextYear");
+const laoding = document.getElementById("loading");
 
 const current_year = new Date().getFullYear();
 
 const new_year_time = new Date(`january 01 ${current_year + 1} 00:00:00`);
+
+// Display next Year
+nextYear.innerText = new_year_time.getFullYear();
+
+//
 
 const update_countdown = function () {
   const current_time = new Date();
@@ -22,7 +28,13 @@ const update_countdown = function () {
   hours.innerText = h < 10 ? "0" + h : h;
   minutes.innerText = m < 10 ? "0" + m : m;
   seconds.innerText = s < 10 ? "0" + s : s;
-  nextYear.innerText = new_year_time.getFullYear();
-  console.log(m);
+  // console.log(m);
 };
+
+// Sowing Spinner before countdown
+setInterval(() => {
+  laoding.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
 setInterval(update_countdown, 1000);
